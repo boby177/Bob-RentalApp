aa199e6f2a0b63865e1308d315c4824608f779fb
boby177 <bomasu37@gmail.com>
2022-12-07 16:50:21 +0700

    fixed bugs from selected params

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -25,12 +25,14 @@ const SearchFilters = () => {
     const values = getFilterValues(filterValues);
 
     values.forEach((item) => {
-      query[item.name] = item.value;
+      if (item.value && filterValues?.[item.name]) {
+        query[item.name] = item.value;
+      }
     });
 
     router.push({ pathname: path, query });
   };
-  
+
   return (
     <Flex bg={"gray.100"} p="4" justifyContent={"center"} flexWrap="wrap">
       {filters?.map((filter) => (
